a3daaf3c696ffffd2baa1ca2377025e31a58a382
Lucas Passos <lucasplpx@gmail.com>
2021-01-26 10:35:18 -0300

    #2 - Comunicação em tempo real: WebRTC, Peer Server e Eventos

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -16,6 +16,18 @@ const onload = () => {
 
   const socketUrl = 'http://localhost:3000';
   const socketBuilder = new SocketBuilder({ socketUrl})
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/'
+    }
+  })
+
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
   const view = new View();
   const media = new Media();
 
@@ -23,7 +35,8 @@ const onload = () => {
     view,
     media,
     room,
-    socketBuilder
+    socketBuilder,
+    peerBuilder
   };
 
   Business.initialize(deps);
